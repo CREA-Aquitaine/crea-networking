4d5592bfdd64211f9a6b76236e6a9368c67f680a
Christellec64 <christelle.cwcs64@gmail.com>
2020-06-11 12:25:17 +0200

    created User_Infos and DashboardTable components

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Container, Row } from 'reactstrap';
 import styles from './Dashboard.module.css';
+import userInfos from './User_Infos';
+import DashboardTable from './Dashboard_table';
 
 function Dashboard() {
   return (
     <Container className={styles.dashboard}>
-      <Row>.row</Row>
-      <Row>.row</Row>
+      <Row>
+        <userInfos />
+      </Row>
+      <Row>
+        <DashboardTable />
+      </Row>
       <Row>.row</Row>
     </Container>
   );
---
+++ src/components/dashboard/User_Infos.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function userInfos() {
+  return <></>;
+}
+
+export default userInfos;
