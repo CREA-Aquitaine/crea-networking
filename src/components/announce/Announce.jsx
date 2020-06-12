cbaecaab05dfee8df35fcc520502b8904bf5e862
Lio-code <lio4455@hotmail.com>
2020-06-12 12:22:32 +0200

    some css

--- src/components/announce/Announce.jsx
+++ src/components/announce/Announce.jsx
@@ -13,13 +13,13 @@ class Annouce extends React.Component {
     return (
       <Container className={styles.zoneglobal}>
         <Row className={styles.zonetop}>
-          <Col className={styles.card}>
+          <Col sm={{ size: 4 }} className={styles.card}>
             <p>ID:</p>
             <p>Ville:</p>
             <p>Secteur d&#39;activité:</p>
           </Col>
 
-          <Col>
+          <Col sm={{ size: 6, offset: 1 }}>
             <h3>TITRE ANNONCE</h3>
             <p>
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Incidunt
@@ -29,11 +29,18 @@ class Annouce extends React.Component {
             </p>
           </Col>
         </Row>
-        <Row>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perspiciatis
-          rem repudiandae voluptate animi eligendi veniam sequi. Consequuntur
-          libero reiciendis quisquam ipsum soluta nemo, earum repellat quibusdam
-          beatae! Doloremque, ullam inventore.
+        <Row className={styles.zonebottom}>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aliquid in
+          impedit placeat! Facilis magnam nam corporis, quas, possimus inventore
+          quidem porro totam similique, et necessitatibus saepe? Corporis
+          expedita aut velit officia cum sequi distinctio esse quibusdam
+          voluptatum consequuntur debitis fuga facere quas ex vel quod modi,
+          voluptas, incidunt, obcaecati quo. Perferendis, ullam fugiat? Pariatur
+          beatae, voluptatem minus ullam voluptate quisquam culpa. Recusandae
+          alias distinctio similique mollitia saepe quasi beatae voluptates quas
+          earum delectus. Animi quae est, eligendi nam vel architecto nobis
+          cumque impedit distinctio cum exercitationem, quo, sunt nesciunt
+          earum.
         </Row>
         <Row className={styles.btn}>
           <button type="button">Postuler</button>
