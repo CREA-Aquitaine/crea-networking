d0db6cc47f8247d506ac0447b4ebdd89053448f6
nanou-11 <anais.jouaret@gmail.com>
2020-06-30 20:02:31 +0200

    change logos to navbar and add photo header

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -3,18 +3,10 @@ import styles from './Dashboard.module.css';
 import UserInfos from './User_Infos';
 import DashboardTable from './Dashboard_table';
 import DashboardBreadcrumb from './DashboardBreadcrumb';
-import banner from '../image/img_header_crea.png';
 
 function Dashboard() {
   return (
     <div className={styles.dashboard}>
-      <img
-        src={banner}
-        alt="header crea"
-        className={styles.banner}
-        width="100%"
-        height="100%"
-      />
       <DashboardBreadcrumb />
       <div className={styles.userInfos}>
         <UserInfos />
