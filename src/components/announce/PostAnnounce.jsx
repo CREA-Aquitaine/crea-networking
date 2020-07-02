5d1de12026db6de5cced4c9a6ca6ab374cdb7daa
Charlyln <charleshenry.lenue>
2020-07-02 10:51:27 +0200

    add axios post in dashboard,and post in postAnnonce

--- src/components/announce/PostAnnounce.jsx
+++ src/components/announce/PostAnnounce.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import {
   Form,
   FormGroup,
@@ -9,19 +9,46 @@ import {
   Row,
   Col,
 } from 'reactstrap';
+import Axios from 'axios';
 
 import PostAnnounceBreadcrumb from './PostAnnounceBreadcrumb';
 
 import styles from './PostAnnounce.module.css';
-import Editor from './Editor';
+// import Editor from './Editor';
 
 function PostAnnounce() {
+  const [content, setContent] = useState('');
+
+  useEffect(() => {}, []);
+
+  const handlePostAnnounce = async (e) => {
+    e.preventDefault();
+    try {
+      await Axios.post('http://localhost:8080/api/v1/posts', {
+        title: 'title',
+        content,
+        localisation: 'Landes',
+        language: 'french',
+        UserId: '0d7315c2-ce56-4c65-a949-4cc82574dddb',
+        JobCategoryId: '7a6ee62c-f115-4546-b15d-432e96de2a47',
+        TypePostId: 'e7763300-ae52-45f4-9463-7fe2480329ff',
+      });
+    } catch (err) {
+      throw new Error(err);
+    }
+  };
+
+  const handleEditorChange = (e) => {
+    setContent(e.target.value);
+  };
+
   return (
     <>
       <Container>
         <PostAnnounceBreadcrumb />
+
         <p className={styles.paragraphLeft}>ID : 40987</p>
-        <Form>
+        <Form onSubmit={handlePostAnnounce}>
           <FormGroup row>
             <Col sm={2} className={styles.paragraphLeft}>
               <Label for="exampleSelect">Type d&lsquo;annonce: </Label>
@@ -47,7 +74,16 @@ function PostAnnounce() {
               />
             </Col>
           </FormGroup>
-          <Editor />
+          {/* <Editor handleEditorChange={() => handleEditorChange()} /> */}
+          <FormGroup>
+            <Input
+              placeholder="Votre annonce"
+              type="textarea"
+              name="text"
+              id="exampleText"
+              onChange={handleEditorChange}
+            />
+          </FormGroup>
           <Row>
             <Col sm={{ size: 1, offset: 9 }}>
               <Button className="button">Valider</Button>
