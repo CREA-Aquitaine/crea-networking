a25416d8cec6b360b19a6430291a706bbafd2059
Christellec64 <christelle.cwcs64@gmail.com>
2020-07-08 18:12:55 +0200

    order by works in Announces list admin

--- src/components/admin/AnnouncesList/Announces_List.jsx
+++ src/components/admin/AnnouncesList/Announces_List.jsx
@@ -1,4 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import {
   Button,
   Breadcrumb,
@@ -12,7 +15,26 @@ import { Link } from 'react-router-dom';
 import styles from './Announces.module.css';
 import AnnouncesListTable from './Announces_List_Table';
 
-function AnnouncesList() {
+function AnnouncesList({ token }) {
+  const [announcesList, setAnnounceslist] = useState([]);
+  const [error, setError] = useState('');
+
+  const getAnnounces = async () => {
+    try {
+      const res = await Axios.get('http://localhost:8080/api/v1/posts', {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      setAnnounceslist(res.data);
+    } catch (err) {
+      setError(error);
+    }
+  };
+
+  useEffect(() => {
+    getAnnounces();
+  }, []);
   return (
     <>
       <Container>
@@ -54,7 +76,7 @@ function AnnouncesList() {
             </Col>
           </Row>
           <Row>
-            <AnnouncesListTable />
+            <AnnouncesListTable announcesList={announcesList} />
           </Row>
           <Row>
             <Col xs="3" className={styles.link}>
@@ -72,4 +94,12 @@ function AnnouncesList() {
   );
 }
 
-export default AnnouncesList;
+const mapStateToProps = (state) => ({
+  token: state.authenticated.token,
+});
+
+AnnouncesList.propTypes = {
+  token: PropTypes.string.isRequired,
+};
+
+export default connect(mapStateToProps)(AnnouncesList);
--- src/components/admin/AnnouncesList/Announces_List_Table.jsx
+++ src/components/admin/AnnouncesList/Announces_List_Table.jsx
@@ -1,38 +1,150 @@
-import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
+
 import { Table, Col, Input } from 'reactstrap';
 
 import styles from './Announces.module.css';
 
-const labelTable = [
-  { label: 'N° Annonce' },
-  { label: 'Titre' },
-  { label: "Type d'annonce" },
-  { label: 'Catégorie' },
-];
+function AnnouncesListTable({ announcesList }) {
+  const [isAsc, setIsAsc] = useState(false);
+  const [numberAnnounce, setNumberAnnounce] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [typeAnnounces, setTypeAnnounces] = useState([]);
+  const [category, setCategory] = useState([]);
 
-function AnnouncesListTable({ token }) {
-  const [announcesList, setAnnounceslist] = useState([]);
-  const [error, setError] = useState('');
-
-  const getAnnounces = async () => {
-    try {
-      const res = await Axios.get('http://localhost:8080/api/v1/posts', {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      });
-      setAnnounceslist(res.data);
-    } catch (err) {
-      setError(error);
-    }
+  const getAscnumberAnnounce = () => {
+    const ascNumberAnnounce = announcesList.sort((a, b) => {
+      const firstNumberAnnounce = a.id.toLowerCase();
+      const secondNumberAnnounce = b.id.toLowerCase();
+      if (firstNumberAnnounce < secondNumberAnnounce) {
+        return -1;
+      }
+      if (firstNumberAnnounce > secondNumberAnnounce) {
+        return 1;
+      }
+      return 0;
+    });
+    setNumberAnnounce(ascNumberAnnounce);
+    setIsAsc(true);
+    return numberAnnounce && isAsc;
   };
 
-  useEffect(() => {
-    getAnnounces();
-  }, []);
+  const getDscnumberAnnounce = () => {
+    const dscNumberAnnounce = announcesList.sort((a, b) => {
+      const firstNumberAnnounce = a.id.toLowerCase();
+      const secondNumberAnnounce = b.id.toLowerCase();
+      if (firstNumberAnnounce > secondNumberAnnounce) {
+        return -1;
+      }
+      if (firstNumberAnnounce < secondNumberAnnounce) {
+        return 1;
+      }
+      return 0;
+    });
+    setNumberAnnounce(dscNumberAnnounce);
+    setIsAsc(false);
+    return numberAnnounce && isAsc;
+  };
+
+  const getAscTitle = () => {
+    const ascTitle = announcesList.sort((a, b) => {
+      const firstTitle = a.title.toLowerCase();
+      const secondTitle = b.title.toLowerCase();
+      if (firstTitle < secondTitle) {
+        return -1;
+      }
+      if (firstTitle > secondTitle) {
+        return 1;
+      }
+      return 0;
+    });
+    setTitle(ascTitle);
+    setIsAsc(true);
+    return title && isAsc;
+  };
+  const getDscTitle = () => {
+    const dscTitle = announcesList.sort((a, b) => {
+      const firstTitle = a.title.toLowerCase();
+      const secondTitle = b.title.toLowerCase();
+      if (firstTitle > secondTitle) {
+        return -1;
+      }
+      if (firstTitle < secondTitle) {
+        return 1;
+      }
+      return 0;
+    });
+    setTitle(dscTitle);
+    setIsAsc(false);
+    return title && isAsc;
+  };
+
+  const getAscTypeAnnounce = () => {
+    const ascTypeAnnounce = announcesList.sort((a, b) => {
+      const firstTypeAnnounce = a.TypePostId.toLowerCase();
+      const secondTypeAnnounce = b.TypePostId.toLowerCase();
+      if (firstTypeAnnounce < secondTypeAnnounce) {
+        return -1;
+      }
+      if (firstTypeAnnounce > secondTypeAnnounce) {
+        return 1;
+      }
+      return 0;
+    });
+    setTypeAnnounces(ascTypeAnnounce);
+    setIsAsc(true);
+    return typeAnnounces && isAsc;
+  };
+  const getDscTypeAnnounce = () => {
+    const dscTypeAnnounce = announcesList.sort((a, b) => {
+      const firstTypeAnnounce = a.TypePostId.toLowerCase();
+      const secondTypeAnnounce = b.TypePostId.toLowerCase();
+      if (firstTypeAnnounce > secondTypeAnnounce) {
+        return -1;
+      }
+      if (firstTypeAnnounce < secondTypeAnnounce) {
+        return 1;
+      }
+      return 0;
+    });
+    setTypeAnnounces(dscTypeAnnounce);
+    setIsAsc(false);
+    return typeAnnounces && isAsc;
+  };
+
+  const getAscCategory = () => {
+    const ascCategory = announcesList.sort((a, b) => {
+      const firstCategory = a.JobCategoryId.toLowerCase();
+      const secondCategory = b.JobCategoryId.toLowerCase();
+      if (firstCategory < secondCategory) {
+        return -1;
+      }
+      if (firstCategory > secondCategory) {
+        return 1;
+      }
+      return 0;
+    });
+    setCategory(ascCategory);
+    setIsAsc(true);
+    return category && isAsc;
+  };
+  const getDscCategory = () => {
+    const dscCategory = announcesList.sort((a, b) => {
+      const firstCategory = a.JobCategoryId.toLowerCase();
+      const secondCategory = b.JobCategoryId.toLowerCase();
+      if (firstCategory > secondCategory) {
+        return -1;
+      }
+      if (firstCategory < secondCategory) {
+        return 1;
+      }
+      return 0;
+    });
+    setCategory(dscCategory);
+    setIsAsc(false);
+    return category && isAsc;
+  };
+
   return (
     <Col>
       <Table borderless>
@@ -41,27 +153,74 @@ function AnnouncesListTable({ token }) {
             <th className={styles.thCheckbox}>
               <Input type="checkbox" className={styles.checkboxTh} />
             </th>
-            {labelTable.map((label) => {
-              return (
-                <th>
-                  {label.label}
-                  <button
-                    type="button"
-                    className={styles.arrowButtons}
-                    onClick={getAnnounces}
-                  >
-                    <span className={styles.arrowDown} />
-                  </button>
-                  <button
-                    type="button"
-                    className={styles.arrowButtons}
-                    onClick={getAnnounces}
-                  >
-                    <span className={styles.arrowUp} />
-                  </button>
-                </th>
-              );
-            })}
+            <th>
+              N° Annonce
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getAscnumberAnnounce}
+              >
+                <span className={styles.arrowDown} />
+              </button>
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getDscnumberAnnounce}
+              >
+                <span className={styles.arrowUp} />
+              </button>
+            </th>
+            <th>
+              Titre
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getAscTitle}
+              >
+                <span className={styles.arrowDown} />
+              </button>
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getDscTitle}
+              >
+                <span className={styles.arrowUp} />
+              </button>
+            </th>
+            <th>
+              Type d&apos;annonces
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getAscTypeAnnounce}
+              >
+                <span className={styles.arrowDown} />
+              </button>
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getDscTypeAnnounce}
+              >
+                <span className={styles.arrowUp} />
+              </button>
+            </th>
+            <th>
+              Catégorie
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getAscCategory}
+              >
+                <span className={styles.arrowDown} />
+              </button>
+              <button
+                type="button"
+                className={styles.arrowButtons}
+                onClick={getDscCategory}
+              >
+                <span className={styles.arrowUp} />
+              </button>
+            </th>
           </tr>
         </thead>
         <tbody>
@@ -83,12 +242,9 @@ function AnnouncesListTable({ token }) {
     </Col>
   );
 }
-const mapStateToProps = (state) => ({
-  token: state.authenticated.token,
-});
 
 AnnouncesListTable.propTypes = {
-  token: PropTypes.string.isRequired,
+  announcesList: PropTypes.string.isRequired,
 };
 
-export default connect(mapStateToProps)(AnnouncesListTable);
+export default AnnouncesListTable;
